53c6f46c22719d5a6abad73706df82caf8c2e6f7
DeveloperRaj <rajchhatrala2001@gmail.com>
2019-10-30 20:57:53 +0530

    bug fix 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
 import Header from './components/Header';
 import SearchUser from './components/SearchUser';
