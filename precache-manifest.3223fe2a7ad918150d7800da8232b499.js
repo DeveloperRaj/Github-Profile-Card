d4489b9e7f4ad9cd6cb444aa254459a7926d9b4b
DeveloperRaj <rajchhatrala2001@gmail.com>
2019-10-30 19:42:17 +0530

    Updates

---
+++ precache-manifest.3223fe2a7ad918150d7800da8232b499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252ee8c6d4e5cb269978dbb9e929b905",
+    "url": "/GitHub-ProfileCard-React/index.html"
+  },
+  {
+    "revision": "6b8dae5458ddffb999b6",
+    "url": "/GitHub-ProfileCard-React/static/css/main.3ea75811.chunk.css"
+  },
+  {
+    "revision": "e9989acee2c5c09b887f",
+    "url": "/GitHub-ProfileCard-React/static/js/2.b738a678.chunk.js"
+  },
+  {
+    "revision": "6b8dae5458ddffb999b6",
+    "url": "/GitHub-ProfileCard-React/static/js/main.94be868e.chunk.js"
+  },
+  {
+    "revision": "06cbd6a535257885c446",
+    "url": "/GitHub-ProfileCard-React/static/js/runtime-main.2fa0662c.js"
+  }
+]);
